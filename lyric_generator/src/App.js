2d32062943f26bc29d8df4947de5c703177acdb7
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-23 21:29:56 -0500

    updated code

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Search from './components/Search';
 import './App.css';
 
 class App extends Component {
---
+++ lyric_generator/src/components/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render () {
+        return (
+            <div className='search'>
+                <input type='text' 
+
+            </div> 
+        )
+    }
+}
