0215baaad8ec6445b2ecdd4b9f46f244c8586bef
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 13:41:14 -0500

    didn't work

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -1,31 +1,10 @@
 import React,{Component} from 'react';
+import {}
 import Search from './components/Search';
 import './App.css';
 
 
 class App extends Component {
-  getLyrics = async (e) => {
-    const searchBar = e.target.elements.searchBar.value;
-    e.preventDefault();
-    const api_call = await fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "genius.p.rapidapi.com",
-        "x-rapidapi-key": "6fef0c97f8msh9ff84b83e67846cp195e8ajsnb541264b0f03"
-      }
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-    
-    const data = await api_call.json();
-    console.log(data)
-
-  }
-
   render() {
     return (
       <div id="app">
