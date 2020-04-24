c40fa45c69f21f0f5987ef8663f4c275869a8959
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 05:18:54 -0500

    a sync

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -2,13 +2,16 @@ import React,{Component} from 'react';
 import Search from './components/Search';
 import './App.css';
 
+const Api_Key = "6fef0c97f8msh9ff84b83e67846cp195e8ajsnb541264b0f03"
+
 class App extends Component {
   getLyrics = (e) => {
     const searchBar = e.target.elements.searchBar.value;
     e.preventDefault();
+    const api_call = "https://genius.p.rapidapi.com/search?key=&q=Kendrick%20Lamar"
     console.log(searchBar);
   }
-  
+
   render() {
     return (
       <div id="app">
