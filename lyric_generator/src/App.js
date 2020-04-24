a4e2ba663788412ecde58c655af73ea9268de944
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 14:12:34 -0500

    whats wrong

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import './App.css';
 
@@ -7,10 +7,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div id="app">
         <nav className="navBar">LYRIC GENERATOR</nav>
         <Search />
       </div>
+      </Router>
      );
    }
 }
--- lyric_generator/src/components/Search.js
+++ lyric_generator/src/components/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Search = () => {
+    return (
         <form className="searchform">
             <input className="searchbar" type="text" placeholder="Enter song and artist name" />
             <button className="searchbutton" type="submit">Search</button>
         </form>
+    )    
 };
 
 export default Search;
