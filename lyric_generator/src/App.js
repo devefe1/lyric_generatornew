da9d48e298778f4dcdc3bc59d9ba89f3f0613886
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 17:35:52 -0500

    updated app.js

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -3,8 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import './App.css';
 
+const API_KEY = "";
 
 class App extends Component {
+  getGif = async (e) => {
+    const gifName = e.target.elements.gifName.value;
+    e.preventDefault();
+    const api_call = await fetch ();
   render() {
     return (
       <Router>
