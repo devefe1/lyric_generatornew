e93b0e7245635821fa196ebf33e6dde7de3f25bc
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 17:49:42 -0500

    stressing out

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -1,25 +1,28 @@
 import React,{Component} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import './App.css';
 
-const API_KEY = "";
+const API_KEY = "D51KB0RSHL0B";
 
 class App extends Component {
   getGif = async (e) => {
     const gifName = e.target.elements.gifName.value;
     e.preventDefault();
-    const api_call = await fetch ();
-  render() {
-    return (
-      <Router>
-      <div id="app">
-        <nav className="navBar">GIF GENERATOR</nav>
-        <Search />
-      </div>
-      </Router>
-     );
-   }
-}
+    const api_call = await fetch (`https://api.tenor.com/v1/search?q=atl&key=${API_KEY}&limit=8`);
+    
+    const data = await api_call.json();
+    console.log(data);
+
+
+    render() {
+      return (
+        <div id="app">
+          <nav className="navBar">GIF GENERATOR</nav>
+          <Search />
+        </div>
+      );
+    };
+  };  
+};
 
 export default App;
--- lyric_generator/src/components/Search.js
+++ lyric_generator/src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Search = () => {
     return (
         <form className="searchform">
-            <input className="searchbar" type="text" placeholder="Search for a gif here! " />
+            <input name="gifName" className="searchbar" type="text" placeholder="Search for a gif here! " />
             <button className="searchbutton" type="submit">Search</button>
         </form>
     )    
