5f78d15d8c0470a32c5122a6cd907795fd7a5aa6
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 18:31:25 -0500

    added new component

--- lyric_generator/src/components/Search.js
+++ lyric_generator/src/components/Search.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Search = () => {
-    return (
-        <form className="searchform">
+const Search = props => (
+        <form onSubmit={props.getGif} className="searchform">
             <input name="gifName" className="searchbar" type="text" placeholder="Search for a gif here! " />
             <button className="searchbutton" type="submit">Search</button>
         </form>
-    )    
-};
+    );
 
 export default Search;
