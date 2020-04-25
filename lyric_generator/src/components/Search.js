dd31cbcd92eea9e4f6afceb40026c6d75d5a32ab
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 22:38:13 -0500

    struggling

--- lyric_generator/src/components/Search.js
+++ lyric_generator/src/components/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Search = () => (
+const Search = () => {
+    return(
         <form className="searchform">
             <input className="searchbar" type="text" placeholder="Search for a gif here! " />
             <button className="searchbutton" type="submit">Search</button>
         </form>
     );
+}
 
 export default Search;
