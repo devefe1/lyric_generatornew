f53f3ee98f12356f2a68bc36d618e8149fc3e70f
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-24 23:08:16 -0500

    fixing code

--- lyric_generator/src/App.js
+++ lyric_generator/src/App.js
@@ -40,8 +40,7 @@ componentDidMount() {
         gifs: grabData.data.results[0].media.url,
         gifs1: grabData.data.results[1],
         gifs2: grabData.data.results[2],
-        gifs3: grabData.data.results[3]
-
+        gifs3: grabData.data.results[3]\
       })
       
     } catch(error) {
